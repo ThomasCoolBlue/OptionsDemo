e6789c74b0a3c5ab920158354a58d4ee1a976863
ThomasCoolblue <thomas.hartman@coolblue.nl>
2026-02-13 16:18:31 +0100

    use valid config by default

--- obj/Debug/net10.0/OptionsDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/OptionsDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OptionsDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51e134bf0444d48f725193c4d048e4104846ed1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("OptionsDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OptionsDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
